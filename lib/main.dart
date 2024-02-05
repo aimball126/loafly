cc5fe3ee12f56cda54bf12e782a1fc55bf2c1020
Ayoub <108541989+aybqam@users.noreply.github.com>
2024-02-06 00:30:03 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
 // يتم استيراد حزم وملفات مختلفة المطلوبة للتطبيق
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:loafly/screens/auth/singup/using_phone.dart';
 import 'package:loafly/screens/auth/welcome_screen.dart';
-import 'package:loafly/screens/root.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
 
 import 'constants/colors.dart';
-
 // دالة البدء الرئيسية للتطبيق
 
 Future<void> main() async {
