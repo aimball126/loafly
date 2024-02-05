f04326762852a7e768b0bcf2747b79a82557f3c0
Ayoub <108541989+aybqam@users.noreply.github.com>
2024-02-06 00:31:49 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -48,3 +48,33 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Dashboard',
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(
+          primaryColor: MaterialColor(
+            primaryColorCode,
+            <int, Color>{
+              50: const Color.fromARGB(255, 212, 159, 14).withOpacity(0.1),
+              100: const Color.fromARGB(255, 212, 159, 14).withOpacity(0.2),
+              200: const Color.fromARGB(255, 212, 159, 14).withOpacity(0.3),
+              300: const Color.fromARGB(255, 212, 159, 14).withOpacity(0.4),
+              400: const Color.fromARGB(255, 212, 159, 14).withOpacity(0.5),
+              500: const Color.fromARGB(255, 212, 159, 14).withOpacity(0.6),
+              600: const Color.fromARGB(255, 212, 159, 14).withOpacity(0.7),
+              700: const Color.fromARGB(255, 212, 159, 14).withOpacity(0.8),
+              800: const Color.fromARGB(255, 212, 159, 14).withOpacity(0.9),
+              900: const Color.fromARGB(255, 212, 159, 14).withOpacity(1.0),
+            },
+          ),
+          scaffoldBackgroundColor: Color(0xFF171821),
+          fontFamily: 'IBMPlexSans',
+          brightness: Brightness.dark),
+      home: DashBoard(),
+    );
+  }
