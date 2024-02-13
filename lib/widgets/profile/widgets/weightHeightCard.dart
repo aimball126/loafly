568d3038e18ab300c56e2ba7fbe24e26923da61f
Ayoub <108541989+aybqam@users.noreply.github.com>
2024-02-13 16:17:20 +0200

    Create weightHeightCard.dart

---
+++ lib/widgets/profile/widgets/weightHeightCard.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dashboard/widgets/custom_card.dart';
+
+class WeightHeightCard extends StatelessWidget {
+  const WeightHeightCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomCard(
+      color: const Color(0xFF2F353E),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          details("Name", "Ayoub"),
+          details("home address", "Janzour"),
+          details("phone", "0914274729"),
+        ],
+      ),
+    );
+  }
+
+  Widget details(String key, String value) {
+    return Column(
+      children: [
+        Text(
+          key,
+          style: const TextStyle(
+            fontSize: 11,
+            color: Colors.grey,
+          ),
+        ),
+        const SizedBox(
+          height: 2,
+        ),
+        Text(
+          value,
+          style: const TextStyle(
+            fontSize: 14,
+          ),
+        ),
+      ],
+    );
+  }
+}
