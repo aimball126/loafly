6607146c6499d7fc0505427e77321577192c4268
Ayoub <108541989+aybqam@users.noreply.github.com>
2024-02-23 04:30:53 +0200

    Update header_widget.dart

--- lib/pages/home/widgets/header_widget.dart
+++ lib/pages/home/widgets/header_widget.dart
@@ -4,9 +4,9 @@ import 'package:flutter_dashboard/responsive.dart';
 
 class Header extends StatelessWidget {
   const Header({
-    super.key,
+    Key? key,
     required this.scaffoldKey,
-  });
+  }) : super(key: key);
 
   final GlobalKey<ScaffoldState> scaffoldKey;
 
@@ -37,53 +37,71 @@ class Header extends StatelessWidget {
               flex: 4,
               child: TextField(
                 decoration: InputDecoration(
-                    filled: true,
-                    fillColor: cardBackgroundColor,
-                    enabledBorder: const OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.transparent),
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12.0),
-                      borderSide:
-                          BorderSide(color: Theme.of(context).primaryColor),
-                    ),
-                    contentPadding: const EdgeInsets.symmetric(
-                      vertical: 5,
-                    ),
-                    hintText: 'Search',
-                    prefixIcon: const Icon(
-                      Icons.search,
-                      color: Colors.grey,
-                      size: 21,
-                    )),
+                  filled: true,
+                  fillColor: cardBackgroundColor,
+                  enabledBorder: const OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.transparent),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                    borderSide:
+                        BorderSide(color: Theme.of(context).primaryColor),
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(
+                    vertical: 5,
+                  ),
+                  hintText: 'Search',
+                  prefixIcon: const Icon(
+                    Icons.search,
+                    color: Colors.grey,
+                    size: 21,
+                  ),
+                ),
               ),
             ),
           if (Responsive.isMobile(context))
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                IconButton(
-                  icon: const Icon(
+            Expanded(
+              flex: 4,
+              child: TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: cardBackgroundColor,
+                  enabledBorder: const OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.transparent),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                    borderSide:
+                        BorderSide(color: Theme.of(context).primaryColor),
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(
+                    vertical: 5,
+                  ),
+                  hintText: 'Search',
+                  prefixIcon: const Icon(
                     Icons.search,
                     color: Colors.grey,
-                    size: 25,
+                    size: 21,
                   ),
-                  onPressed: () {},
                 ),
-                InkWell(
-                  onTap: () => scaffoldKey.currentState!.openEndDrawer(),
-                  child: CircleAvatar(
-                    backgroundColor: Color.fromARGB(255, 255, 255, 255),
-                    child: Image.asset(
-                      "assets/images/avatar.png",
-                      width: 32,
-                    ),
-                  ),
-                )
-              ],
+              ),
+            ),
+          if (Responsive.isMobile(context))
+            InkWell(
+              onTap: () => scaffoldKey.currentState!.openEndDrawer(),
+              child: CircleAvatar(
+                backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+                child: Image.asset(
+                  "assets/images/avatar.png",
+                  width: 32,
+                ),
+              ),
             ),
         ],
       ),
