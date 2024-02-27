c7be99e73de7acee96c8b4a4d59c2fb04a35d013
Ayoub <108541989+aybqam@users.noreply.github.com>
2024-02-28 00:44:40 +0200

    Delete lib/constants directory

--- lib/constants/images.dart
+++
@@ -1,5 +0,0 @@
-class LoafImages{
-  static const String welcome_bg = "assets/images/welcome.jpg";
-  static const String phoneReg_screen = "assets/images/phone_reg.jpg";
-  static const String libyanFlag = "assets/images/libyan_flag.png";
-}
--- lib/constants/texts.dart
+++
@@ -1,9 +0,0 @@
-
-class ConstTexts{
-  //General Texts
-  static const String appName = "Loaf.ly";
-  //Welcome Screen
-  static const String welcomeText = "مرحباََ بك في تطبيق";
-  static const String slogan = "خبزتك لباب حوشك";
-  static const String appUsing = "الدخول بإستخدام";
-}
