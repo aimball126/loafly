f4d2c1dbb8113556b6ca8aed08fcac29270708ab
Ayoub <108541989+aybqam@users.noreply.github.com>
2024-03-11 18:09:48 +0200

    Update activity_details_card.dart

--- lib/activity_details_card.dart
+++ lib/activity_details_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter_dashboard/custom_card.dart';
 import 'package:flutter_dashboard/const.dart';
 import 'package:flutter_dashboard/responsive.dart';
@@ -111,6 +112,65 @@ class Header extends StatelessWidget {
   }
 }
 
+
+class PriceCard {
+  final String value;
+  final double price;
+  const PriceCard({required this.value, required this.price});
+}
+
+class DetailsCard extends StatelessWidget {
+  const DetailsCard({super.key});
+
+  final List<PriceCard> healthDetails = const [
+    PriceCard(value: "Day", price: 25.99),
+    PriceCard(value: "Week", price: 30.99),
+    PriceCard(value: "Month", price: 70.99),
+    PriceCard(value: "Year", price: 150.99),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      itemCount: healthDetails.length,
+      shrinkWrap: true,
+      physics: const ScrollPhysics(),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: Responsive.isMobile(context) ? 2 : 4,
+          crossAxisSpacing: !Responsive.isMobile(context) ? 15 : 12,
+          mainAxisSpacing: 12.0),
+      itemBuilder: (context, i) {
+        return CustomCard(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 15, bottom: 4),
+                child: Text(
+                  healthDetails[i].value,
+                  style: const TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600),
+                ),
+              ),
+              Text(
+                '\$${healthDetails[i].price.toStringAsFixed(2)}',
+                style: const TextStyle(
+                    fontSize: 13,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.normal),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+
 class BreadModel {
   final String icon;
   final String value;
@@ -154,33 +214,10 @@ class BreadModel {
     price: 25.99,
   );
 
-  static const BreadModel bread3 = BreadModel(
-    icon: 'assets/images/Candies.png',
-    value: "حلويات",
-    quantity: "الكمية",
-    value2: "2",
-    delivery: "الزمن",
-    data: "وقت 15-20 دقيقة",
-    amount: "المبلغ",
-    price: 25.99,
-  );
-
-  static const BreadModel bread4 = BreadModel(
-    icon: 'assets/images/Crackers.png',
-    value: "المقرمشات",
-    quantity: "الكمية",
-    value2: "5",
-    delivery: "الزمن",
-    data: "وقت 15-20 دقيقة",
-    amount: "المبلغ",
-    price: 20.99,
-  );
 
   static const List<BreadModel> breadDetails = [
     bread1,
     bread2,
-    bread3,
-    bread4,
   ];
 }
 
@@ -334,6 +371,180 @@ class ActivityDetailsCard extends StatelessWidget {
           ],
         );
       },
+    );
+  }
+}
+
+class LineChartCard extends StatelessWidget {
+  LineChartCard({super.key});
+
+  final List<FlSpot> spots = const [
+    FlSpot(1.68, 21.04),
+    FlSpot(2.84, 26.23),
+    FlSpot(5.19, 19.82),
+    FlSpot(6.01, 24.49),
+    FlSpot(7.81, 19.82),
+    FlSpot(9.49, 23.50),
+    FlSpot(12.26, 19.57),
+    FlSpot(15.63, 20.90),
+    FlSpot(20.39, 39.20),
+    FlSpot(23.69, 75.62),
+    FlSpot(26.21, 46.58),
+    FlSpot(29.87, 42.97),
+    FlSpot(32.49, 46.54),
+    FlSpot(35.09, 40.72),
+    FlSpot(38.74, 43.18),
+    FlSpot(41.47, 59.91),
+    FlSpot(43.12, 53.18),
+    FlSpot(46.30, 90.10),
+    FlSpot(47.88, 81.59),
+    FlSpot(51.71, 75.53),
+    FlSpot(54.21, 78.95),
+    FlSpot(55.23, 86.94),
+    FlSpot(57.40, 78.98),
+    FlSpot(60.49, 74.38),
+    FlSpot(64.30, 48.34),
+    FlSpot(67.17, 70.74),
+    FlSpot(70.35, 75.43),
+    FlSpot(73.39, 69.88),
+    FlSpot(75.87, 80.04),
+    FlSpot(77.32, 74.38),
+    FlSpot(81.43, 68.43),
+    FlSpot(86.12, 69.45),
+    FlSpot(90.06, 78.60),
+    FlSpot(94.68, 46.05),
+    FlSpot(98.35, 42.80),
+    FlSpot(101.25, 53.05),
+    FlSpot(103.07, 46.06),
+    FlSpot(106.65, 42.31),
+    FlSpot(108.20, 32.64),
+    FlSpot(110.40, 45.14),
+    FlSpot(114.24, 53.27),
+    FlSpot(116.60, 42.13),
+    FlSpot(118.52, 57.60),
+  ];
+
+  final leftTitle = {
+    0: '0',
+    20: '2K',
+    40: '4K',
+    60: '6K',
+    80: '8K',
+    100: '10K'
+  };
+  final bottomTitle = {
+    0: 'Jan',
+    10: 'Feb',
+    20: 'Mar',
+    30: 'Apr',
+    40: 'May',
+    50: 'Jun',
+    60: 'Jul',
+    70: 'Aug',
+    80: 'Sep',
+    90: 'Oct',
+    100: 'Nov',
+    110: 'Dec',
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    // Replace `fixedColor` with your desired color
+    Color fixedColor = Colors.blue;
+
+    return CustomCard(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            "General consumption",
+            style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+          ),
+          const SizedBox(height: 20),
+          AspectRatio(
+            aspectRatio: Responsive.isMobile(context) ? 9 / 4 : 16 / 6,
+            child: LineChart(
+              LineChartData(
+                lineTouchData: LineTouchData(handleBuiltInTouches: true),
+                gridData: FlGridData(show: false),
+                titlesData: FlTitlesData(
+                  bottomTitles: AxisTitles(
+                    sideTitles: SideTitles(
+                      showTitles: true,
+                      reservedSize: 32,
+                      interval: 1,
+                      getTitlesWidget: (double value, TitleMeta meta) {
+                        return bottomTitle[value.toInt()] != null
+                            ? SideTitleWidget(
+                                axisSide: meta.axisSide,
+                                space: 10,
+                                child: Text(
+                                  bottomTitle[value.toInt()].toString(),
+                                  style: TextStyle(
+                                    fontSize: Responsive.isMobile(context) ? 9 : 12,
+                                    color: Colors.grey[400],
+                                  ),
+                                ),
+                              )
+                            : const SizedBox();
+                      },
+                    ),
+                  ),
+                  rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                  topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                  leftTitles: AxisTitles(
+                    sideTitles: SideTitles(
+                      getTitlesWidget: (double value, TitleMeta meta) {
+                        return leftTitle[value.toInt()] != null
+                            ? Text(
+                                leftTitle[value.toInt()].toString(),
+                                style: TextStyle(
+                                  fontSize: Responsive.isMobile(context) ? 9 : 12,
+                                  color: Colors.grey[400],
+                                ),
+                              )
+                            : const SizedBox();
+                      },
+                      showTitles: true,
+                      interval: 1,
+                      reservedSize: 40,
+                    ),
+                  ),
+                ),
+                borderData: FlBorderData(show: false),
+                lineBarsData: [
+                  LineChartBarData(
+                    isCurved: true,
+                    curveSmoothness: 0,
+                    color: fixedColor, // Use the fixed color here
+                    barWidth: 2.5,
+                    isStrokeCapRound: true,
+                    belowBarData: BarAreaData(
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          fixedColor.withOpacity(0.5), // Use the fixed color here
+                          Colors.transparent,
+                        ],
+                      ),
+                      show: true,
+                      color: fixedColor.withOpacity(0.5), // Use the fixed color here
+                    ),
+                    dotData: FlDotData(show: false),
+                    spots: spots,
+                  ),
+                ],
+                minX: 0,
+                maxX: 120,
+                maxY: 105,
+                minY: -5,
+              ),
+              swapAnimationDuration: const Duration(milliseconds: 250),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
